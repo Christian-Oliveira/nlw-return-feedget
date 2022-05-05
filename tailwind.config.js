937eb6e8cfd6aa33d1515ea988348641700038a2
Christian Oliveira <christianoliveirati@gmail.com>
2022-05-05 18:58:27 -0300

    config tailwind and crate button feedback with acessibility

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          400: '#996DFF',
+          500: '#8257E6'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
